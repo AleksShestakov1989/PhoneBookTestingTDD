00fe07aa508e8c8a3cf86a7443a51226b6dc3f12
Aleks <shestakov.aleksey8989@gmail.com>
2023-07-19 00:24:55 +0300

    add implemented.

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -5,6 +5,11 @@ public class PhoneBook {
     private static Map<String, String> phoneBook = new HashMap<>();
 
     public static int add(String name, String number) {
-        return 0;
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, number);
+        } else {
+            System.out.println("Contact repeat");
+        }
+        return phoneBook.size();
     }
 }
