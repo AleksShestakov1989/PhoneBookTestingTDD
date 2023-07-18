630137d80b20a688da53b4e455d548aace7003f0
Aleks <shestakov.aleksey8989@gmail.com>
2023-07-19 00:23:07 +0300

    Stub and tests for add.

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private static Map<String, String> phoneBook = new HashMap<>();
+
+    public static int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    private static PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void addRepeatContact() {
+        final int expected = 2;
+        phoneBook.add("One", "111");
+        phoneBook.add("Two", "222");
+        final int actual = phoneBook.add("One", "333");
+        assertEquals(expected, actual);
+    }
 }
